6f71b5d486afaae49ce351d46b21332d363ac30f
Matt Mower <mowerm@gmail.com>
2013-12-20 12:07:24 -0600

    Update output_styles script

--- output_styles.sh
+++ output_styles.sh
@@ -14,19 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ ! -f "./styles/README.md" ]; then
+if [ ! -d "styles" ]; then
     git clone https://github.com/citation-style-language/styles.git styles
-fi
-if [ -d "./styles" ]; then
+    cd styles
+else
     cd styles
     git pull origin master
-    ls *.csl |
-    sed 's/\.[^.]*$//' | # remove extension
-    sed 's/\(.*\)/"\1"/g' | # wrap each style in double quotes
-    tr '\n' ',' | # replace line-breaks with commas
-    sed 's/\(.\).$/\1];/g' | # replace last comma with: ];
-    sed 's/^/var\ allStyles\ =\ [/g' > ../cite_styles.js # insert variable name and [ at start
+fi
+if [ -f "README.md" ]; then
+	 # remove existing styles
+	sed -i '$ d' ../cite_styles.js
+#              remove extension     wrap styles in quotes   \n --> ,      last comma --> ];        insert variable name and [       append styles
+    ls *.csl | sed 's/\.[^.]*$//' | sed 's/\(.*\)/"\1"/g' | tr '\n' ',' | sed 's/\(.\).$/\1];/g' | sed 's/^/var\ allStyles\ =\ [/g' >> ../cite_styles.js
+	cd ..
+	rm -rf styles    
 else
-    echo "Unable to clone styles repository."
+    echo "Styles repository unavailable"
 fi
 

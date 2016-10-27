a5d0615593a1e65dd969f9976bfb4e49a6430cab
Matt Mower <mowerm@gmail.com>
2016-10-26 22:54:20 -0500

    Update acknowledgements and copyright year

--- output_styles.sh
+++ output_styles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2015 Matthew D. Mower
+# Copyright (C) 2016 Matthew D. Mower
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

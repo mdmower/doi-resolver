ad33b247dc8f7e236a45ab0556b802ce916fb2e2
Matt Mower <mowerm@gmail.com>
2018-07-19 03:58:57 -0700

    Autolink: Use local storage

--- autolink.js
+++ autolink.js
@@ -14,56 +14,39 @@
 	limitations under the License.
 */
 
-storage();
+definitions();
+beginInit();
 
-function storage() {
-	if (typeof storage.area === 'undefined') {
-		storage.area = chrome.storage.local;
-	}
-	if (typeof storage.urlPrefix === 'undefined') {
-		storage.urlPrefix = "https://dx.doi.org/";
-	}
-	if (typeof storage.findDoi === 'undefined') {
-		// https://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
-		storage.findDoi = /\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b/ig;
-	}
-	if (typeof storage.findUrl === 'undefined') {
-		// https://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
-		storage.findUrl = /^(?:https?\:\/\/)(?:dx\.)?doi\.org\/(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)$/ig;
-	}
-	if (typeof storage.autolinkRewrite === 'undefined') {
-		storage.autolinkRewrite = false;
-	}
+function definitions() {
+    definitions.urlPrefix = "https://dx.doi.org/";
+    // https://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
+    definitions.findDoi = /\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b/ig;
+    definitions.findUrl = /^(?:https?\:\/\/)(?:dx\.)?doi\.org\/(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)$/ig;
+    definitions.autolinkRewrite = false;
+}
 
-	chrome.storage.local.get(["sync_data"], function(stg) {
-		if (stg.sync_data === true) {
-			storage.area = chrome.storage.sync;
-		} else {
-			storage.area = chrome.storage.local;
+function beginInit() {
+	var stgFetch = [
+		"cr_autolink",
+		"auto_link_rewrite",
+		"custom_resolver",
+		"doi_resolver"
+	];
+
+	chrome.storage.local.get(stgFetch, function(stg) {
+		if (stg.custom_resolver === true && stg.cr_autolink == "custom") {
+			definitions.urlPrefix = stg.doi_resolver;
+			definitions.autolinkRewrite = stg.auto_link_rewrite === true;
 		}
-
-		var stgFetch = [
-			"cr_autolink",
-			"auto_link_rewrite",
-			"custom_resolver",
-			"doi_resolver"
-		];
-
-		storage.area.get(stgFetch, function(stg) {
-			if (stg.custom_resolver === true && stg.cr_autolink == "custom") {
-				storage.urlPrefix = stg.doi_resolver;
-				storage.autolinkRewrite = stg.auto_link_rewrite === true;
-			}
-			replaceDOIsWithLinks();
-		});
+		replaceDOIsWithLinks();
 	});
 }
 
 // https://stackoverflow.com/questions/1444409/in-javascript-how-can-i-replace-text-in-an-html-page-without-affecting-the-tags
 function replaceDOIsWithLinks() {
-	replaceInElement(document.body, storage.findDoi, function(match) {
+	replaceInElement(document.body, definitions.findDoi, function(match) {
 		var link = document.createElement('a');
-		link.href = storage.urlPrefix + match[0];
+		link.href = definitions.urlPrefix + match[0];
 		link.appendChild(document.createTextNode(match[0]));
 		return link;
 	});
@@ -78,9 +61,9 @@ function replaceInElement(element, find, replace) {
 		if (child.nodeType === Node.ELEMENT_NODE) {
 			if (forbiddenTags.indexOf(child.nodeName.toLowerCase()) < 0) {
 				replaceInElement(child, find, replace);
-			} else if (storage.autolinkRewrite && child.nodeName.toLowerCase() == "a") {
-				if (storage.findUrl.test(child.href)) {
-					child.href = child.href.replace(storage.findUrl, storage.urlPrefix + "$1");
+			} else if (definitions.autolinkRewrite && child.nodeName.toLowerCase() == "a") {
+				if (definitions.findUrl.test(child.href)) {
+					child.href = child.href.replace(definitions.findUrl, definitions.urlPrefix + "$1");
 				}
 			}
 		} else if (child.nodeType === Node.TEXT_NODE) {

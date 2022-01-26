c86a129c68d7549c27a59708773ffd67172f7d6a
Matt Mower <mowerm@gmail.com>
2022-01-25 20:57:30 -0800

    Remove deprecated scripts
    `npm run update-csl` replaces updated-styles.sh and update-locales.sh
    `npm run pkg` replaces zip_extension.sh

--- update-locales.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# SPDX-License-Identifier: Apache-2.0
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-wget -O "$SCRIPT_DIR/../src/csl/locales/README.md" https://github.com/citation-style-language/locales/raw/master/README.md
-wget -O "$SCRIPT_DIR/../src/csl/locales/locales.json" https://github.com/citation-style-language/locales/raw/master/locales.json

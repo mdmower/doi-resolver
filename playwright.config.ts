b87fb0367d94ba48b2f07896b48a8521530946ef
Matt Mower <mowerm@gmail.com>
2024-09-28 10:58:26 -0700

    Allow tests to run in parallel
    Fixtures seem to be isolated correctly even in full parallel mode. This
    speeds up test execution significantly.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import {defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: 'line',
